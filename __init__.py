5135dcc007c21ffda35e67c0dbbca9d366fe40ab
lancelotnd <32873563+lancelotnd@users.noreply.github.com>
2017-10-19 20:04:29 -0400

    Test Voice Response app route /

--- __init__.py
+++ __init__.py
@@ -1,34 +1,14 @@
-from flask import Flask, request, Response
-from twilio.rest import Client
+from flask import Flask
+from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
-# Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC190046e06b0612f0f1be5beab91f7030"
-auth_token  = "{a0e68c354c1a4d54021a99800a031040}"
-workspace_sid = "{WS7049aaac0bb91a16bda876a28d16a4a7}"
-workflow_sid = "{WW25179b77ce4678c8d7a30f2c35a35f15}"
+@app.route("/", methods=['GET', 'POST'])
+def welcome():
+    resp = VoiceResponse()
+    resp.say("Welcome Lancelot")
 
-client = Client(username=account_sid, password=auth_token)
-
-@app.route("/assignment_callback", methods=['GET', 'POST'])
-def assignment_callback():
-    """Respond to assignment callbacks with empty 200 response"""
-
-    resp = Response({}, status=200, mimetype='application/json')
-    return resp
-
-@app.route("/create_task", methods=['GET', 'POST'])
-def create_task():
-    """Creating a Task"""
-    task = client.taskrouter.workspaces(sid=workspace_sid)\
-                 .tasks.create(workflow_sid=workflow_sid,
-                               attributes='{"selected_language":"es"}')
-
-    print(task.attributes)
-    resp = Response({}, status=200, mimetype='application/json')
-    return resp
+    return str(resp)
 
 if __name__ == "__main__":
     app.run()
-
