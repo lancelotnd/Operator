20f07931adfc0442daa78ae460c85b8671850473
lancelotnd <32873563+lancelotnd@users.noreply.github.com>
2017-10-25 16:12:00 -0400

    conf

--- __init__.py
+++ __init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
-from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.voice_response import Conference, Dial, VoiceResponse
+from twilio.rest import Client
+
+account_sid = "AC190046e06b0612f0f1be5beab91f7030"
+auth_token = "a0e68c354c1a4d54021a99800a031040"
+client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def welcome():
-    resp = VoiceResponse()
-    resp.say("Welcome Lancelot")
 
-    return str(resp)
+@app.route("/voice", methods=['GET', 'POST'])
+def conference():
+    response = VoiceResponse()
+    dial = Dial()
+    dial.conference('Room 1234')
+    response.append(dial)
+
+    return str(response)
+
 
 if __name__ == "__main__":
     app.run()
