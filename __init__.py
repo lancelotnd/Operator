17cd0a5c3510eecf37e90214e962b19d56221b3f
lancelotnd <32873563+lancelotnd@users.noreply.github.com>
2017-10-26 16:45:08 -0400

    TaskRouter

--- __init__.py
+++ __init__.py
@@ -1,23 +1,13 @@
-from flask import Flask
-from twilio.twiml.voice_response import Conference, Dial, VoiceResponse
-from twilio.rest import Client
-
-account_sid = "AC190046e06b0612f0f1be5beab91f7030"
-auth_token = "a0e68c354c1a4d54021a99800a031040"
-client = Client(account_sid, auth_token)
+from flask import Flask, Response
+import json
 
 app = Flask(__name__)
 
-
-@app.route("/voice", methods=['GET', 'POST'])
-def conference():
-    response = VoiceResponse()
-    dial = Dial()
-    dial.conference('moderated-conference-room', start_conference_on_enter=False)
-    response.append(dial)
-
-    return str(response)
-
+@app.route('/assignment_callback', methods=['POST','GET'])
+def assignment_callback():
+    """For now, we will respond to assignment callbacks with empty 200 response"""
+    resp = Response({}, status = 200, mimetype = 'application/json')
+    return resp
 
 if __name__ == "__main__":
     app.run()
